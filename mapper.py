4ee069ad8224aa5c13b776e6695bc6f8af4dbcd7
sammiechum <147017220+sammiechum@users.noreply.github.com>
2023-10-06 18:03:38 -0400

    Add files via upload

---
+++ mapper.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  5 22:38:03 2023
+
+@author: sammiechum
+"""
+import sys, re
+import gzip
+import tarfile
+import os
+import string
+library = []
+
+def clean_text(text):
+    text = str(text).lower()
+    text = re.sub('\[.*?\]', '', text)
+    text = re.sub('[%s]' % re.escape(string.punctuation), ' ', text)
+    text = re.sub('[\d\n]', ' ', text)
+    return text
+
+
+def determineValence(content):
+    cleaned = clean_text(content)
+    words = cleaned.split()
+    for word in words:
+        for i in range (len(library)):
+            if (library[i]["word"] == word):
+                key = os.environ['mapreduce_map_input_file']
+                key = word
+                value = library[i]["value"]
+                print(key,"\t",value)
+
+
+def main(argv):
+    # line = sys.stdin.readline()
+    with open(argv[2], 'r') as file:
+        for line in file:
+            words = line.split()
+            dict = {
+                "word": words[0],
+                "value": words[1] }
+            library.append(dict)
+        # print(library)
+            
+    for gz_file in os.listdir(argv[1]):
+        pathtofile = os.path.join(argv[1],gz_file)
+        
+        # print(pathtofile)
+        with gzip.open(pathtofile, 'rb') as gz_file:
+        # Read the contents of the compressed file
+            # print(gz_file)
+            with tarfile.open(fileobj= gz_file, mode='r') as tar:
+            # List the contents of the tar archive (optional)
+                # print(tar.list())
+                for txt in tar.getmembers():
+                    speech = tar.extractfile(txt)
+                    if speech:
+                        content = speech.read()
+                        determineValence(content)
+                        # print(content.decode('utf-8'))
+
+if __name__ == "__main__":
+    main(sys.argv)
---
+++ reducer.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+"""reducer.py"""
+
+from operator import itemgetter
+import sys
+
+current_word = None
+current_totalValence = 0
+word = None
+count = 0
+
+# input comes from STDIN
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+
+    # parse the input we got from mapper.py
+    word, valence = line.split('\t', 1)
+
+    # convert count (currently a string) to int
+    try:
+        valence = int(count)
+    except ValueError:
+        # count was not a number, so silently
+        # ignore/discard this line
+        continue
+    # this IF-switch only works because Hadoop sorts map output
+    # by key (here: word) before it is passed to the reducer
+    if current_word == word:
+        current_totalValence += valence
+        count += 1
+    else:
+        if current_word:
+            # write result to STDOUT
+            average = current_totalValence/count
+            print ('%s\t%s' % (current_word, average))
+            # print ('%s\t%s' % (current_word, current_count))
+        current_count = count
+        current_word = word
+    
+    
+# # do not forget to output the last word if needed!
+if current_word == word:
+    print ('%s\t%s' % (current_word, current_count))
